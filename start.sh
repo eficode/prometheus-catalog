eb97a4cb4346d003dc6a9195415ba7d74a5346f5
Matej Cotman <matej.cotman@eficode.com>
2020-11-20 15:47:53 +0200

    fix(#2): implement locking mechanism

--- start.sh
+++ start.sh
@@ -5,8 +5,10 @@ service nginx start
 export DB_FILE="${DB_FILE:-/var/prometheus-catalog/db.json}"
 export FILE_SD_CONFIG="${FILE_SD_CONFIG:-/var/prometheus-catalog/file_sd_config.json}"
 mkdir -p "$(dirname $DB_FILE)"
+chown -R www-data:www-data "$(dirname $DB_FILE)"
 touch "$DB_FILE"
 chown www-data:www-data "$DB_FILE"
+rm -f "${DB_FILE}.lock"  # since start.sh is ran on start of all processes we can delete lock file
 touch "$FILE_SD_CONFIG"
 chown www-data:www-data "$FILE_SD_CONFIG"
 
